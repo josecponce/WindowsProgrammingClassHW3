e877700559761ce22679838ea12e4acc0c1e901c
Jose C Ponce <pepote5391@gmail.com>
2016-09-18 14:55:05 -0400

    Implemented the OathUserControl in the control library.

---
+++ Homework3/Dialogs/OathDialog/OathDialog.cs
@@ -0,0 +1,18 @@
+﻿using Homework3ControlLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3.Dialogs.OathDialog {
+    public partial class OathDialog : DialogBase {
+        public OathDialog() {
+            InitializeComponent();
+        }
+    }
+}
